229e4777c709bcf5c8f1881ee946737683306d64
Jakk Sut <jakks@Jakks-iMac.local>
2017-09-21 00:48:11 +0700

    add calendar, moment

---
+++ components/Calendar/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
+
+// eslint-disable-next-line
+class Calendar extends React.Component {
+  render() {
+    moment.locale('th');
+    moment().format('LL');
+    return (
+      <BigCalendar
+        {...this.props}
+        events={[
+          {
+            title: this.props.workdays[0].timeCode,
+            allDay: true,
+            start: this.props.workdays[0].workday,
+            end: this.props.workdays[0].workday
+          },
+          {
+            title: this.props.workdays[1].timeCode,
+            allDay: true,
+            start: this.props.workdays[1].workday,
+            end: this.props.workdays[1].workday
+          }
+        ]}
+        views={['month']}
+        defaultDate={new Date()}
+      />
+    );
+  }
+}
+
+Calendar.defaultProps = {
+  timeCode: '-'
+};
+
+Calendar.propTypes = {
+  workdays: PropTypes.array.isRequired,
+  timeCode: PropTypes.string
+};
+
+export default Calendar;
--- routes.js
+++ routes.js
@@ -13,10 +13,9 @@ const routes = require('next-routes')();
 //
 // ------------ ROUTES ---------------
 // @RANStartRoutes
-routes.add('details', '/details/:postId/:postTitle');
-routes.add('create', '/create_post');
 routes.add('signin', '/sign_in');
 routes.add('signup', '/sign_up');
+routes.add('userProfile', '/profile');
 // @RANEndRoutes
 // ------------ ROUTES ---------------
 //
